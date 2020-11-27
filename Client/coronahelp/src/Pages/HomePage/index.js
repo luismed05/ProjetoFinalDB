1ceb2b7ea1f1042d953d7d730cf54b69819fe382
luismed <luis_medeiros96@yahoo.com.br>
2020-11-27 20:01:00 -0300

    🔧 Modal para o Chamado de emergencia

--- Client/coronahelp/src/Pages/HomePage/index.js
+++ Client/coronahelp/src/Pages/HomePage/index.js
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { GetUser, Logout, Auth, GetAllUsers, DelUser } from "../../Utils/api";
-import { Modal, Table, Card, Form, Button } from 'react-bootstrap';
+import { Modal, Table, Card, Form, Row, Button } from 'react-bootstrap';
 import useForceUpdate from 'use-force-update';
 import "./Lading.css";
 
@@ -11,6 +11,7 @@ export default function HomePage() {
     const forceUpdate = useForceUpdate();
     let history = useHistory();
     const [show, setShow] = useState(false);
+    const [showCall, setshowCall] = useState(false);
 
     const [User,setUser] = useState({email: "",nome: "", admin: ""});
     const [Users,setUsers] = useState([]);
@@ -18,16 +19,17 @@ export default function HomePage() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const handleCloseCall = () => setshowCall(false);
+    const handleShowCall = () => setshowCall(true);
+
     useEffect(() => {
         let email = localStorage.getItem("userEmail");
         let token = localStorage.getItem("token");
         Auth(email,token)
         .then(res => {
             if(res.data.access == true){
-                console.log(res)
                 GetUser(email)
                 .then(res => {
-                    console.log(res)
                     setUser({
                         email: res.data[0].email,
                         nome: res.data[0].nome,
@@ -54,12 +56,10 @@ export default function HomePage() {
     const DeletarUsuario = async(email) => {
         await DelUser(email)
         .then(res => {
-            console.log(res)
             alert("usuario deletado com sucesso");
             window.location.reload();
         })
         .catch(err => {
-            console.log(err.data)
             alert("erro ao deletar usuario");
         })
     }
@@ -72,6 +72,49 @@ export default function HomePage() {
     }
   return(
       <div>
+          <Modal className="ModalCallEmergency" size="lg" show={showCall} onHide={handleCloseCall}>
+              <Card className="cardFormCall">
+                  <Card.Title>Informe o Paciente:</Card.Title>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Cpf:</Form.Label>
+                        <Form.Control type="number" placeholder="Ex. xxx.xxx.xxx-xx"></Form.Control>
+                        <Form.Label>Nome do Paciente:</Form.Label>
+                        <Form.Control></Form.Control>
+                        <Form.Group>
+                            <Form.Label>Genero:</Form.Label>
+                            <div style={{marginLeft: '1vw',display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <Form.Check inline name="Genero" type="radio" label="Masculino" />
+                                <Form.Check inline name="Genero" type="radio" label="Feminino" />
+                                <Form.Check inline name="Genero" type="radio" label="Não desejo informar" />
+                            </div>
+                        </Form.Group>
+                        <Form.Label>Data de Nascimento:</Form.Label>
+                        <Form.Control></Form.Control>
+                        <div className="PesoAltura">
+                            <Form.Group>
+                                <Form.Label>Peso:</Form.Label>
+                                <Form.Control type="number" placeholder="ex. 98,5"></Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Altura:</Form.Label>
+                                <Form.Control type="number" placeholder="ex. 1,87" ></Form.Control>
+                            </Form.Group>
+                        </div>
+                        <Form.Label>Selecione os sintomas:</Form.Label>
+                        <div style={{marginLeft: '1vw'}}>
+                            <Form.Check label="Febre" />
+                            <Form.Check label="Tosse Seca" />
+                            <Form.Check label="Cansaço" />
+                            <Form.Check label="Dificuldade de Respirar ou falta de ar" />
+                            <Form.Check label="Dor ou pressão no peito" />
+                            <Form.Check label="Perda de fala ou movimento" />
+                        </div>
+                    </Form.Group>
+                    <Button type="submit">Fazer Chamado</Button>
+                </Form>
+              </Card>
+          </Modal>
           <Modal size="lg" show={show} onHide={handleClose}>
               <Table style={{maxWidth: "70vw"}}>
                   <thead>
@@ -120,7 +163,7 @@ export default function HomePage() {
             </div>
         </div>
         <div className="Conteudo" >
-            <div className="BigButton" onClick={() => alert("funcionou")}>
+            <div className="BigButton" onClick={handleShowCall}>
                 <span>Fazer Chamado</span>
             </div>
         </div>
