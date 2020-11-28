9242a05d265db8a953573d5f00f4f4d664f79b5b
luismed <luis_medeiros96@yahoo.com.br>
2020-11-28 12:09:54 -0300

    🔨 Adição de rotas para o cadastro de paciente

--- Server/Rotas/Routes.js
+++ Server/Rotas/Routes.js
@@ -2,12 +2,18 @@ module.exports = app => {
 
     const Usuarios = require("../App/Controllers/usuario.controller.js");
 
+    const Pacientes = require("../App/Controllers/paciente.controller");
+
     const Proprietarios = require("../App/Controllers/proprietario.controller.js");
 
+    //Rotas padrões de Usuarios
     app.get('/users', Usuarios.index)
     app.post('/SignUp', Usuarios.create)
     app.get('/user/:email', Usuarios.show)
     app.delete('/user/:email', Usuarios.delete)
+
+    //Rotas padrões de Pacientes
+    app.post('/Cadpaciente', Pacientes.create);
 
     //Rotas para autenticação de usuario
     app.post('/Signin', Usuarios.login)
